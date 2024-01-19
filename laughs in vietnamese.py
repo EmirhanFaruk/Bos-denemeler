0d6df26f60372e7de69d56b60fa33bb4e0adc81a
EmirhanFaruk <emirhansagir74@gmail.com>
2024-01-19 18:43:13 +0100

    Adding project files

---
+++ laughs in vietnamese.py
@@ -0,0 +1,47 @@
+
+# Modification date: Thu Jan 27 22:50:32 2022
+
+# Production date: Sun Sep  3 15:42:59 2023
+
+class Tree:
+  def __init__(self, val = None):
+    if val != None:
+        self.val = val
+    else:
+        self.val = None
+    self.left = None
+    self.right = None
+
+  def insert(self, val):
+    if self.val:
+        if val < self.val:
+            if self.left is None:
+                self.left = Tree(val)
+            else:
+                self.left.insert(val)
+        elif val > self.val:
+            if self.right is None:
+                self.right = Tree(val)
+            else:
+                self.right.insert(val)
+    else:
+        self.val = val
+
+  def printValues(self):
+    
+    if self.left:
+        self.left.printValues()
+    print(self.val)
+    if self.right:
+        self.right.printValues()
+    
+
+tree = Tree(20)
+tree.left = Tree(18)
+tree.right = Tree(22)
+tree.insert(19)
+tree.insert(24)
+tree.insert(5)
+tree.insert(21)
+
+tree.printValues()
